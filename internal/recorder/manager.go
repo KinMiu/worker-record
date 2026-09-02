2c0cc41ce7bc0cdffd70c838a944715957307413
KinMiu <prasetyoaldi875@gmail.com>
2026-09-02 12:46:12 +0700

    first commit

--- internal/recorder/manager.go
+++ internal/recorder/manager.go
@@ -196,7 +196,7 @@ func (rm *RecorderManager) runDeviceLoop(ctx context.Context, dev models.Device)
 			"-hide_banner",
 			"-loglevel", "warning",
 			"-rtsp_transport", "tcp",
-			"-stimeout", "5000000", // 5 seconds socket timeout in microseconds
+			"-timeout", "5000000", // 5 seconds socket timeout in microseconds (FFmpeg 5+/6+/7+)
 			"-fflags", "+genpts+nobuffer",
 			"-avoid_negative_ts", "make_zero",
 			"-i", rtspURL,
